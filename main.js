d68400c87f42a1a04598e907a6c87ee0c8b882b1
aishah21 <aishah-almaghrabi@hotmail.com>
2018-11-13 21:52:24 +0300

    finidhed homework

---
+++ main.js
@@ -0,0 +1,53 @@
+//Biggest Numbers 1
+
+function maxOfTwoNumbers (number1 , number2) {
+    if(number1 > number2){
+        console.log('the maximam number is ' , number1);
+    } else {
+        console.log('the maximam number is ' , number2)
+    }
+}
+maxOfTwoNumbers(6,7);
+
+//Biggest Numbers 2 
+
+function maxOfThree(number1 , number2 , number3){
+    console.log('the max numof three is ',Math.max(number1, number2, number3));
+}
+maxOfThree(7,9,12);
+
+//The World Translator
+function languageCode(laCode){
+    if(laCode ==='arabic'){
+        console.log('مرحبا بالعالم');
+    } if(laCode === 'english'){
+        console.log('Hello World');
+    } else if(laCode == 'french') {
+        console.log('Bonjour le monde');
+    }
+
+}
+languageCode('arabic');
+languageCode('english');
+languageCode('french');
+
+//The Grade Assigner
+function assignGrade(score){
+    if( score >= 90){
+        console.log('A');
+    }else if(score >= 80){
+        console.log('B'); 
+    }else if(score >= 70){
+        console.log('C');
+    }else if(score >= 60){
+        console.log('F');
+    }
+    
+}
+assignGrade(78);
+
+//The Pluralizer
+function pluralize(noun,number){
+    console.log(`${number} ${noun}`);
+}
+pluralize('dog',4);
