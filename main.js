8cdf138098bca38320970a48aecbca6b40a66fb1
Shahad AlMohssen <shuunz120@gmail.com>
2018-11-14 10:55:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,27 @@ maxOfTwoNumbers(1,2);
 maxOfTwoNumbers(2,1);
 maxOfTwoNumbers(2,2);
 
+var maxOfThree = function (num1,num2,num3) {
+if ((num1 === num2) && (num2 === num3)) {
+        console.log("numbers are equal");
+  }
+   else {
+    if (num1 > num2 && num1 > num3)
+  {
+    console.log("The greater number is ", num1 );
+}
+
+else if (num2 > num1 && num2 > num3){
+    console.log("The greater number is ", num2 );
+}
+
+else{
+    console.log("The greater number is ", num3 );
+}}
+}
+
+maxOfThree(3,2,1); 
+
 //The World Translator
 
 function helloWorld(langCode){
