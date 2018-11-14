b46344caa223c755dc96d481fd95e8b08c4d1553
marwah14 <memo_224@live.com>
2018-11-14 09:24:58 +0300

    finished homework

---
+++ function.js
@@ -0,0 +1,40 @@
+var mathMax = 
+maxOfTwoNumbers =
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 || num2) {
+        consol.log("Mathmax(num1, num2)");
+
+} else {
+console.log("nothing");
+}
+};
+maxOfTwoNumbers(2, 4);
+
+var helloWorld = function (english, arabic, frensh) {
+    if (english) {
+        console.log("helloworld");
+    } else if (arabic) {
+        console.log("مرحبا");
+    } else if (frensh) {
+        console.log("Bonjour le monde");
+    }
+};
+
+function helloWorld(frensh);
+
+
+var assignGrade = ['A', 'B', 'C', 'D']; 
+
+function assignGrade = {
+    for (var i = 0; i < assignGrade; i++) {
+        if (assignGrade[i] >= 90) {
+            return A;
+        } else if (assignGrade[i] >= 80) {
+            return B;
+        } else if (assignGrade[i] >= 70) {
+            return C;
+        } else if (assignGrade[i] >= 60) {
+            return D;
+        }
+    }
+};
