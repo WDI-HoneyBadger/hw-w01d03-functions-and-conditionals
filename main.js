e68985550f6a81909c55ba015650d059ed2c335d
Layalkashgari <Layalkash@gmail.com>
2018-11-14 11:32:14 +0300

    add homework

---
+++ main.js
@@ -0,0 +1,43 @@
+// Define a function maxOfTwoNumbers that takes 
+//two numbers as arguments and returns the 
+//largest of them. Use the if-then-else construct available in Javascript.
+
+// Define a function maxOfThree that takes three numbers as arguments and returns the largest of them. 
+
+function maxOfTwoNumbers (bigNum, smallNum) {
+    if((bigNum>smallNum)
+  {
+    console.log("This is the largest number");
+} else 
+    console.log("This is not the largest number");
+}
+
+function maxOfThree (num1, num2, num3) {
+if((num1 == 1) 
+    console.log('you can drive')
+  } else {
+    console.log("you can't drive");
+  }
+}
+
+
+function maxOfThree (Languages) {
+    if((Language == English) 
+        console.log ("Hello World");
+      } 
+      else if (language == Arabic)
+    
+      console.log { ("مرحبا بالعالم");
+      }
+      else if (language == "french") {
+        console.log("Bonjour le monde");
+      }
+    }
+
+    HelloWord (English);
+    HelloWord (Arabic);
+    HelloWord (French);
+    
+
+    function assignGrade(score){
+
