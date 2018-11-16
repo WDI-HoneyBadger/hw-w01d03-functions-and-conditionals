e28ac8145480eeb26e8d7e055e52a7baa8df9e79
Masarah Fallath <masarah@MacBook-Air-alkhas-b-Masarah.local>
2018-11-16 17:52:46 +0300

    homework

---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+function maxOfTwoNumbers(one, two){
+    Math.max(one, two);
+    console.log(Math.max(one, two))
+}
+maxOfTwoNumbers(3, 4);
+
+function maxOfThree(num1, num2, num3){
+    console.log(num1, num2, num3)
+}
+
+var score
+var assignGrade = function (score)
+{ console.log(score)};
+//score = assignGrade(score);
+if (score> 90 ) { return assignGrade + "grade = A"
+}
+else if (score > 80) {return grade= "B"
+ } else if (score > 70) {return grade= "C"
+        } else if (score > 60) {return grade= "D"
+    } else {return score= "F"}
+            
+assignGrade(33);
+
+function pluralize(noun, number) { 
+
+    var newletter = [ ];
+    var letter = 0; 
+    if (letter < noun.length-1, letter++ ); {
+        newletter.push("s")
+        return number + noun;
+
+    }
+
+}
+console.log(pluralize(noun, number))
+pluralize(Cat, 5);
