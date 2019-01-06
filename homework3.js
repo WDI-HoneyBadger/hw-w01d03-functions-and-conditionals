0367eeeab1e15b4480f67d489f24ecfbd11fafca
Turki Alomari <t-matar@windowslive.com>
2019-01-06 10:06:20 +0300

    Hw

---
+++ homework3.js
@@ -0,0 +1,21 @@
+var number1
+var number2
+
+function maxOfTwoNumbers(number1, number2) {
+    if (number1 > number2);
+    
+}
+
+else if { 
+    (number1 == number2)
+
+    console.log(number2)
+}
+
+function maxOfThree (1, 2, 3,) {
+    if (1==1)
+}
+
+else { 
+
+}
